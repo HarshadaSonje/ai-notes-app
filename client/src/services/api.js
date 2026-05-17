370081e1e894bd16cd5f4233840a27a01d90ba83
HarshadaSonje <hps250804@gmail.com>
2026-05-17 12:25:32 +0530

    Updated backend URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: "https://ai-notes-backend-l1mw.onrender.com"
 });
 
 API.interceptors.request.use((req) => {
